f0cf620f6b535199ef3b95aa0eb4019a36e54e10
Monishaxworkz <91608064+Monishaxworkz@users.noreply.github.com>
2021-12-03 10:13:41 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,33 @@
+class Sub1{
+              public static void main(String[]a){
+              int num1 = 20;
+              int num2 = 30;
+              int sum = num1-num2;
+             System.out.println("The result" +sum);
+
+            float d = 2;
+            float b =5;
+            float add = d-b;
+           System.out.println("The result" +add);
+
+          //byte e = 10;
+         //byte f = 20;
+        //byte ADD =e-f;
+       //System.out.println("The result"+ADD);
+
+       //short numa = 15;
+      //short numb = 15;
+     //short all =numa-numb;
+    //System.out.println("The result" +all);
+
+    long an = 13;
+   long bn = 14;
+   long cn = an-bn;
+  System.out.println("The result" +cn);
+
+  double ln = 72;
+ double fn = 23;
+double nn = ln-fn;
+System.out.println("The result" +nn);
+}
+}
